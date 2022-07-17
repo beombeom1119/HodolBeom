2e32acce6dce9f23e9a2241a2964cbaceac6d40b
beombeom1119 <qjarl1119@naver.com>
2022-07-17 14:05:27 +0900

    HodolMan 시작

---
+++ src/test/java/com/hodollecture/HodolBeom/HodolBeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodollecture.HodolBeom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodolBeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
